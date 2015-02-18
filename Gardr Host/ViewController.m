3e5b9aef137a157efdf894c1023213b4203e0be2
johsoren <johan.sorensen@finn.no>
2015-02-18 12:16:48 +0100

    demonstrate opening onclick events from webview

--- Gardr Host/ViewController.m
+++ Gardr Host/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#define SUPPORT_LEGACY_LINK_FORMAT 1
+
 @interface ViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *addressTextField;
@@ -37,12 +39,41 @@
 
 #pragma mark - UIWebViewDelegate
 
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+#if SUPPORT_LEGACY_LINK_FORMAT
+    NSString *js = @"var open_ = window.open; \n"
+    "window.open = function(url, name, properties)  \n"
+    "{   \n"
+    "if(name == 'new_window'){ \n"
+    "    var prefix = 'openexternal:';\n"
+    "    var address = url; \n"
+    "    open_(prefix + address); \n"
+    "} \n"
+    "    return open_(address, name, properties); \n"
+    "};";
+
+    [webView stringByEvaluatingJavaScriptFromString:js];
+#endif
+}
+
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
         [[UIApplication sharedApplication] openURL:request.URL];
         return NO;
     }
+
+#if SUPPORT_LEGACY_LINK_FORMAT
+    NSString *urlString = [[request URL] absoluteString];
+    if ([urlString rangeOfString:@"openexternal:"].location != NSNotFound) {
+        NSString *urlWithoutPrefix = [urlString stringByReplacingOccurrencesOfString:@"openexternal:" withString:@""];
+        if (urlWithoutPrefix) {
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlWithoutPrefix]];
+        }
+        return NO;
+    }
+#endif
 
     return YES;
 }
